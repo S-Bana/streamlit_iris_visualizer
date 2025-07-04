f96146c01450565921ffa225b8bb2c595a3430c4
Sajad Bana Zadeh <sajadbanazadeh@gmail.com>
2025-07-04 14:44:59 +0330

    Prediction and show result in streamlit web page

--- app.py
+++ app.py
@@ -20,9 +20,22 @@ df, target_names = load_data()
 model = RandomForestClassifier()
 model.fit(df.iloc[:,:-1], df['species'])
 
+
 #‌ Streamlit design setting
 st.sidebar.title("Input Features")
 sepal_lenght = st.sidebar.slider("sepal length (cm)", float(df["sepal length (cm)"].min()), float(df["sepal length (cm)"].max()))
 sepal_width = st.sidebar.slider("sepal width (cm)", float(df["sepal width (cm)"].min()), float(df["sepal width (cm)"].max()))
 petal_lenght = st.sidebar.slider("petal length (cm)", float(df["petal length (cm)"].min()), float(df["petal length (cm)"].max()))
 petal_width = st.sidebar.slider("petal width (cm)", float(df["petal width (cm)"].min()), float(df["petal width (cm)"].max()))
+
+
+# Prediction
+input_list = [[sepal_lenght, sepal_width, petal_lenght, petal_width]]
+prediction = model.predict(input_list)
+
+prediction_species = target_names[prediction[0]]
+
+
+# prediction result show
+st.write("Prediction")
+st.write(f"Prediction species is : {prediction_species}")
