64e4d645b344b970958491ee80bc5b894fae6e33
Sajad Bana Zadeh <sajadbanazadeh@gmail.com>
2025-07-04 14:22:28 +0330

    write and test function load dataset iris

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
+
+# function load dataset
+def load_data():
+    iris = load_iris()
+    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    df['species'] = iris.target
+    return df, iris.target_names
+
+_,_ = load_data()
